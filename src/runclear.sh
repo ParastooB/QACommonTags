8c86a0d829cc718976e3ee52e4a3ba2629fdf1bd
Parastoo <par_94@yahoo.com>
2018-07-16 16:32:50 -0400

    code

--- src/runclear.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo java -cp ".:../libs/htmlunit-2.27-OSGi.jar:../libs/json-simple-1.1.1.jar:../libs/mysql-connector-java-5.1.42-bin.jar" Clear ../questions/test-TagMe.txt
-java -cp ".:../libs/htmlunit-2.27-OSGi.jar:../libs/json-simple-1.1.1.jar:../libs/mysql-connector-java-5.1.42-bin.jar" Clear ../questions/test-TagMe.txt
